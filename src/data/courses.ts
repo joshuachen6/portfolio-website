bb67a7e8d72a7730f2df0a3f8fb22735514562b6
joshuachen6 <jhc3628@gmail.com>
2026-05-30 00:49:45 -0400

    removed ip

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -15,10 +15,10 @@ export const courseGroups: CourseGroup[] = [
     courses: [
       { code: 'CS 225', title: 'Data Structures & Algorithms' },
       { code: 'CS 340', title: 'Introduction to Computer Systems' },
-      { code: 'CS 374', title: 'Algorithms & Models of Computation', inProgress: true },
+      { code: 'CS 374', title: 'Algorithms & Models of Computation' },
       { code: 'CS 415', title: 'Game Development' },
-      { code: 'CS 411', title: 'Database Systems', inProgress: true },
-      { code: 'CS 427', title: 'Software Engineering I', inProgress: true },
+      { code: 'CS 411', title: 'Database Systems' },
+      { code: 'CS 427', title: 'Software Engineering I' },
     ],
   },
   {
@@ -27,7 +27,7 @@ export const courseGroups: CourseGroup[] = [
       { code: 'MATH 257', title: 'Linear Algebra w/ Computation' },
       { code: 'MATH 285', title: 'Differential Equations' },
       { code: 'CS 357', title: 'Numerical Methods I' },
-      { code: 'BIOE 205', title: 'Signal Processing in BIOE', inProgress: true },
+      { code: 'BIOE 205', title: 'Signal Processing in BIOE' },
     ],
   },
 ];
